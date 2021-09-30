5465721489bf5005b1436d4ade114f1789ef2e85
iagardi <garbigz@gmail.com>
2021-09-30 17:00:34 +0100

    app created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import headerLogo from './assets/images/brewdog_header_logo.jpg';
+import './App.scss';
+
+const App = () => {
+  return <>
+    <div className="app">
+      <header></header>
+      <nav></nav>
+      <main></main>
+      </div>
+  </>
+};
+
+export default App;
